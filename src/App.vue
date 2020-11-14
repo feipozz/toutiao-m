4c957db31cdca7fdfb86ef4177b68c43d466bb6e
feipozz <278366950@qq.com>
2020-11-14 22:42:27 +0800

    项目初始化 引入vant 组件库

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,12 @@
     <!-- 路由出口 -->
     <router-view />
     <h1>黑马头条</h1>
+    <div>
+      <i class="toutiao toutiao-yanzhengma"></i>
+      <van-button type="primary">主要按钮</van-button>
+      <van-cell title="单元格" value="内容" size="large" />
+<van-cell title="单元格" value="内容" size="large" label="描述信息" />
+    </div>
   </div>
 </template>
 <script>
